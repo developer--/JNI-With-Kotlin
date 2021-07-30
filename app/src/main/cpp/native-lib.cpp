73ff7ea91e92aaa78fb4a70f5500346569fcbae3
Jemo <jemo.mgebrishvili@gmail.com>
2021-07-30 13:38:00 +0400

    answer from c++

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,13 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_dev_nativedemo_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+Java_com_dev_nativedemo_MainActivity_stringFromJNI(JNIEnv* env,jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_dev_nativedemo_MainActivity_answerFromJNI(JNIEnv* env,jobject) {
+    int answer = 42;
+    return answer;
 }
